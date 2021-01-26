123f386c9691962609f0870eab63adde78f9654a
Karol <Kskwierawski12@gmail.com>
2021-01-26 17:22:55 +0100

    added unsplash api and random photos

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import NaviBar from "./NaviBar";
+import '../css/styl.css'
 
-export default function Body() {
-
+export default function Body(props) {
 
     return (
-        <div className="container">
-            <hr/>
-        </div>
+        <>
+            <NaviBar/> <br/><br/>
+            <div className="w-100">
+                <div className="row justify-content-md-center">
+                    {props.photos.map((data, index) => {
+                            return (
+                                <div className="col-3 m-2 ">
+                                    <img src={data.urls.small}/>
+                                </div>
+                            )
+                        })}
+                </div>
+            </div>
+        </>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,66 @@
-import React from "react"
+import React, {useState, useEffect, useRef} from "react"
 import NaviBar from "./NaviBar";
 import Footer from "./Footer";
 import Body from "./Body";
-import './style.css'
-
+import '../css/styl.css'
+import nodeFetch from 'node-fetch';
+import {createStore} from "redux";
+import {createApi} from 'unsplash-js';
+import {Nav} from "react-bootstrap";
 
 export default function Home() {
+    const [randomPhotos, SetRandomPhotos] = useState();
+    const keyWord = useRef();
+    const numberOfResults = useRef(10);
+
+    const unsplash = createApi({
+        accessKey: 'ot1DbXB4IikmR2QOHGacf4ADbnOauST1OrwTH3lNBPc',
+        fetch: nodeFetch,
+    });
+
+
+    const getPhoto = async e => {
+        e.preventDefault();
+        let x = await unsplash.search.getPhotos({
+            query: keyWord.current.value,
+            page: 1,
+            perPage: numberOfResults.current.value,
+            orientation: 'portrait',
+        });
+        console.log(x.response.results);
+        SetRandomPhotos(x.response.results);
+
+    }
+
+    async function getRandom() {
+        let x = await unsplash.photos.getRandom({
+            count: 10,
+            orientation: 'portrait',
+        });
+        SetRandomPhotos(x.response);
+    }
+
+    useEffect(() => {
+        (getRandom())
+    }, []);
+
+
     return (
         <>
+            <NaviBar/>
             <meta charSet="utf-8"/>
-        <div className="jumbotron">
-            <img className="_29Gk3" role="presentation" src="https://images.unsplash.com/photo-1610823231022-5d1c830a6e96?ixid=MXwxMjA3fDB8MHxwaG90by1vZi10aGUtZGF5fHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format%2Ccompress&amp;fit=crop&amp;w=1000&amp;h=1000"/>
-        </div>
-        <Body/>
-        <NaviBar/>
-        <Footer/>
+            <div className="jumbotron">
+                <div className="container justify-content-center d-flex">
+                    <form onSubmit={getPhoto} className="w-25 justify-content-center text-center">
+                        <input type="number" ref={numberOfResults} placeholder="Ile zdjęć checsz wyszukiwać" required/>
+                        <input type="text" className="m-2 w-75" placeholder="Słowo klucz" ref={keyWord} required/>
+                        <button className="btn-primary">Wyszukaj zdjęcia</button>
+                    </form>
+                </div>
+            </div>
+            {randomPhotos === undefined ? null : <Body photos={randomPhotos}/>}
+
+            <Footer/>
         </>
     )
 }
