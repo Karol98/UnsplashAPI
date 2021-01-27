5508db70263358b6ba1d56fb479da62ab7a7d4bd
Karol <Kskwierawski12@gmail.com>
2021-01-27 21:59:48 +0100

    general style of the website improved

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,49 +4,62 @@ import Footer from "./Footer";
 import Body from "./Body";
 import '../css/styl.css'
 import nodeFetch from 'node-fetch';
-import {createStore} from "redux";
 import {createApi} from 'unsplash-js';
-import {Nav} from "react-bootstrap";
+import logo from '../img/logo.png';
 
 export default function Home() {
+
     const [randomPhotos, SetRandomPhotos] = useState();
     const keyWord = useRef();
-    const numberOfResults = useRef(10);
+    const numberOfResultsRandom = useRef();
+    const numberOfResults = useRef();
 
     const unsplash = createApi({
         accessKey: 'ot1DbXB4IikmR2QOHGacf4ADbnOauST1OrwTH3lNBPc',
         fetch: nodeFetch,
     });
 
+    async function getRandom(e) {
+        e.preventDefault();
+        let y=10;
+        if(numberOfResultsRandom.current.value !== "") {
+            y = numberOfResultsRandom.current.value;
+        }
+        let x = await unsplash.photos.getRandom({
+            count: y,
+            mode: 'no-cors',
+            orientation: 'portrait',
+        });
+        SetRandomPhotos(x.response);
+    };
 
     const getPhoto = async e => {
         e.preventDefault();
-        console.log("szukam zdjęć");
+        let y=10;
+        if(numberOfResults.current.value >= 0 && numberOfResults.current.value < 100) {
+            y = numberOfResults.current.value;
+        }
         let x = await unsplash.search.getPhotos({
             query: keyWord.current.value,
             page: 1,
-            perPage: numberOfResults.current.value,
+            perPage: y,
             orientation: 'portrait',
         });
-        console.log(x.response.results);
-        SetRandomPhotos(x);
+        SetRandomPhotos(x.response.results);
+    };
 
-    }
-
-    async function getRandom() {
-        console.log("ładuje losowe zdjęcia")
+    async function initPagePhotos() {
         let x = await unsplash.photos.getRandom({
             count: 10,
             mode: 'no-cors',
             orientation: 'portrait',
         });
         SetRandomPhotos(x.response);
-    }
+    };
 
-    useEffect((e) => {
-        (getRandom(e))
+    useEffect(() => {
+        (initPagePhotos())
     }, []);
-
 
     return (
         <>
@@ -54,17 +67,19 @@ export default function Home() {
             <meta charSet="utf-8"/>
             <div className="jumbotron">
                 <div className="container justify-content-center d-flex">
+                    <img src={logo} alt="logo" className="logoUnsplash"/>
                     <form onSubmit={getPhoto} className="w-25 justify-content-center text-center">
-                        <input type="number" ref={numberOfResults} placeholder="Ile zdjęć checsz wyszukiwać" required/>
-                        <input type="text" className="m-2 w-75" placeholder="Słowo klucz" ref={keyWord} required/>
-                        <button className="btn-primary">Wyszukaj zdjęcia</button>
+                        <input type="number" className="form-control" ref={numberOfResults} placeholder="Ile zdjęć checsz wyszukiwać" />
+                        <input type="text" className="mt-2 form-control" placeholder="Słowo klucz" ref={keyWord} required/>
+                        <button className="btn-primary mt-4">Wyszukaj zdjęcia</button>
                     </form>
                 </div>
             </div>
             {randomPhotos === undefined ? null : <Body photos={randomPhotos}/>}
             <div className="d-flex justify-content-center">
                 <form onSubmit={getRandom}>
-                    <button className="btn-primary">Przewijaj dalej</button>
+                    <input type="number" className="form-control" ref={numberOfResultsRandom} placeholder="Ilość losowych zdjęć"/>
+                    <button className="mt-3 btn-primary form-control">Przewijaj dalej</button>
                 </form>
             </div>
             <Footer/>
--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -10,7 +10,7 @@ export default function NaviBar() {
     const [user] = useAuthState(auth);
     return (
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-            <a className="navbar-brand" href="#">UnSplash</a>
+            <a className="navbar-brand" href="#">UnSplashApi</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
                     aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"/>
