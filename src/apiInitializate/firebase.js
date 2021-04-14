49dcbe4c7be94eecae2f328dfbd1ca312c7ab4ef
Karol98 <44203620+Karol98@users.noreply.github.com>
2021-04-14 02:12:16 +0200

    Update firebase.js

--- src/apiInitializate/firebase.js
+++ src/apiInitializate/firebase.js
@@ -2,12 +2,7 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyBNgk24NV6xJwS0Rl0Xs5plAuZfga1X84o",
-    authDomain: "quizbiu.firebaseapp.com",
-    projectId: "quizbiu",
-    storageBucket: "quizbiu.appspot.com",
-    messagingSenderId: "512126771398",
-    appId: "1:512126771398:web:dee367788bedbde98cb7c7"
+    //Put your api Key here
 })
 
 export const auth = app.auth()
